ce3de913d4c47dbefd683df74bf2ea3d52e33006
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-08-22 16:00:08 -0300

    :tada: Commit inicial

---
+++ src/main/java/com/connectCare/connectCareApi/ConnectCareApiApplication.java
@@ -0,0 +1,13 @@
+package com.connectCare.connectCareApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectCareApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectCareApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/connectCare/connectCareApi/ConnectCareApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.connectCare.connectCareApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConnectCareApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

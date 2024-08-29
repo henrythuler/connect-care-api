acbccb6b4fa1df8d14460e02a1706d8f3386ae60
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-08-29 16:56:00 -0300

    :sparkles: feat: add repository medico

---
+++ src/main/java/com/connectCare/connectCareApi/repositories/MedicoRepository.java
@@ -0,0 +1,11 @@
+package com.connectCare.connectCareApi.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.connectCare.connectCareApi.models.entities.Medico;
+
+@Repository
+public interface MedicoRepository extends JpaRepository<Medico, Long> {
+
+}

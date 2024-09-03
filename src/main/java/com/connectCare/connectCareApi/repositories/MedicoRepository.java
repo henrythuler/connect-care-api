9e3daa6ab6b71330b8256e0c874ef8ac85f6db1f
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-09-03 10:16:43 -0300

    :ok_hand: fix: subtituindo o tipo Long por Integer

--- src/main/java/com/connectCare/connectCareApi/repositories/MedicoRepository.java
+++ src/main/java/com/connectCare/connectCareApi/repositories/MedicoRepository.java
@@ -6,6 +6,6 @@ import org.springframework.stereotype.Repository;
 import com.connectCare.connectCareApi.models.entities.Medico;
 
 @Repository
-public interface MedicoRepository extends JpaRepository<Medico, Long> {
+public interface MedicoRepository extends JpaRepository<Medico, Integer> {
 
 }

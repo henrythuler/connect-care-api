d34679610d67fb292de176480cff9879ff0ed7ff
Gleison Fernandes da Silva <gleison.fernandesb@gmail.com>
2024-09-03 10:19:15 -0300

    :sparkles: feat: add MedicoServiceImpl

---
+++ src/main/java/com/connectCare/connectCareApi/services/impl/MedicoServiceImpl.java
@@ -0,0 +1,46 @@
+package com.connectCare.connectCareApi.services.impl;
+
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.connectCare.connectCareApi.models.entities.Medico;
+import com.connectCare.connectCareApi.repositories.MedicoRepository;
+import com.connectCare.connectCareApi.services.GenericService;
+
+@Service
+public class MedicoServiceImpl implements GenericService<Medico> {
+	
+	@Autowired
+	private MedicoRepository repository;
+
+	@Override
+	public Medico create(Medico medico) {
+		return repository.save(medico);
+	}
+
+	@Override
+	public Medico getById(Integer id) {
+		return repository.findById(id).orElseThrow(() -> new RuntimeException("Médico não encontrado"));
+	}
+
+	@Override
+	public List<Medico> getAll() {
+		return repository.findAll();
+	}
+
+	@Override
+	public Medico update(Medico medico) {
+		Medico medicoEncontrado = repository.getReferenceById(medico.getId());
+        BeanUtils.copyProperties(medico, medicoEncontrado);
+        return repository.save(medicoEncontrado);
+	}
+
+	@Override
+	public void delete(Integer id) {
+		repository.deleteById(id);
+	}
+	
+}

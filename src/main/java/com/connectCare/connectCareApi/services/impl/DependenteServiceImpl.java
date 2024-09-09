9842a9aabe451d8931130d0931362ac18e1f8836
Henry Thuler <henrythuler2003@outlook.com>
2024-09-09 10:02:01 -0300

    :sparkles: feat: Service básico de Dependente

---
+++ src/main/java/com/connectCare/connectCareApi/services/impl/DependenteServiceImpl.java
@@ -0,0 +1,44 @@
+package com.connectCare.connectCareApi.services.impl;
+
+import com.connectCare.connectCareApi.models.entities.Dependente;
+import com.connectCare.connectCareApi.repositories.DependenteRepository;
+import com.connectCare.connectCareApi.services.GenericService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class DependenteServiceImpl implements GenericService<Dependente> {
+
+    @Autowired
+    private DependenteRepository repository;
+
+    @Override
+    public Dependente create(Dependente dependente) {
+        return repository.save(dependente);
+    }
+
+    @Override
+    public Dependente getById(Integer id) {
+        return repository.findById(id).orElseThrow(() -> new RuntimeException("Dependente não encontrado..."));
+    }
+
+    @Override
+    public List<Dependente> getAll() {
+        return repository.findAll();
+    }
+
+    @Override
+    public Dependente update(Dependente dependente) {
+        Dependente dependenteEncontrado = repository.getReferenceById(dependente.getId());
+        BeanUtils.copyProperties(dependente, dependenteEncontrado);
+        return repository.save(dependenteEncontrado);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        repository.deleteById(id);
+    }
+}
